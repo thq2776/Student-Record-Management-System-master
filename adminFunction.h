23facc7f10325db8c2b5383a6c1cc57522f138f1
Trần Ngọc Thanh <56815987+thq2776@users.noreply.github.com>
2024-04-10 13:39:57 +0700

    Update adminFunction.h
    View
    Update
    Delete

--- adminFunction.h
+++ adminFunction.h
@@ -364,6 +364,119 @@ void updateData()
 }
 
 */
+void addData()
+{
+    system("cls");
+    fstream file("data.csv", ios::out | ios::app);
+    if (!file)
+    {
+        cout << "\nFailed to open data.csv / data.csv file not found!";
+    }
+
+    else
+        cout << "\t|Enter Data|" << endl
+             << endl;
+
+    string rollNumber, name, college, branch, attendance, physics, chemistry, maths;
+    string moreData;
+
+    cout << "\nEnter Student's roll Number : ";
+    cin >> rollNumber;
+    file << "\n"
+         << rollNumber << ", ";
+
+    cout << "\nEnter Studnet's Name : ";
+    cin >> name;
+    file << name << ", ";
+
+    cout << "\nEnter College : ";
+    cin >> college;
+    file << college << ", ";
+
+    cout << "\nEnter Branch : ";
+    cin >> branch;
+    file << branch << ", ";
+
+    cout << "\nEnter Attendance : ";
+    cin >> attendance;
+    file << attendance << ", ";
+
+    cout << "\nEnter Physic's Marks : ";
+    cin >> physics;
+    file << physics << ", ";
+
+    cout << "\nEnter Chemistry Marks : ";
+    cin >> chemistry;
+    file << chemistry << ", ";
+
+    cout << "\nEnter Maths Marks : ";
+    cin >> maths;
+    file << maths;
+
+    file.close();
+
+    cout << "\nAdd more Record? (Y / N) : ";
+
+    cin >> moreData;
+    if (moreData == "y" || moreData == "Y" || moreData == "yes" || moreData == "Yes")
+    {
+        addData();
+    }
+    else if (moreData == "n" || moreData == "N" || moreData == "no" || moreData == "No")
+        adminFunction();
+
+    else
+        cout << "\nEnter a valid option";
+
+    adminFunction();
+}
+
+void viewData()
+{
+    system("cls");
+
+    fstream file("data.csv");
+
+    if (!file)
+    {
+        cout << "fail in opening the file";
+    }
+    cout << "\n\t\t\t\t\t\t|Students Record| \n\n";
+
+    cout << "\n-----------------------------------------------------------------------------------------------------------------------\n"
+         << endl
+         << endl;
+    cout << "Roll \t Name \t\t\t College \t branch \t Attendance \t Physics \t Chemistry \t Maths" << endl
+         << endl;
+    string rollNumber, name, college, branch, attendance, physics, chemistry, maths;
+
+    while (!file.eof()) // file.eof() = detects as how long the f
+    {
+        getline(file, rollNumber, ',');
+        getline(file, name, ',');
+        getline(file, college, ',');
+        getline(file, branch, ',');
+        getline(file, attendance, ',');
+        getline(file, physics, ',');
+        getline(file, chemistry, ',');
+        getline(file, maths, '\n');
+
+        cout << rollNumber << " \t " << name << "\t\t\t" << college << " \t\t" << branch << " \t\t" << attendance << " \t\t" << physics << " \t\t" << chemistry << " \t\t" << maths << endl;
+        cout << endl;
+    }
+
+    cout << "\n-----------------------------------------------------------------------------------------------------------------------" << endl;
+    file.close();
+
+    cout << "\n\n 1. Add Record\n";
+    cout << " 2. Delete Record\n";
+    cout << " 3. Update Record\n";
+    cout << " 5. Main Menu\n";
+    cout << " 6. Exit\n";
+    cout << "\nEnter choice : ";
+    // adminFunction();
+}
+
 
 void adminVerification()
 {
